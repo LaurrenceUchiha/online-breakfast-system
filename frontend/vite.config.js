2da595185e84d5e6069319299e8f8107673eb857
Laurrence Kim <23-40299@g.batstate-u.edu.ph>
2025-11-04 14:27:08 +0800

    d

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite';
-
 export default defineConfig({
   root: '.',
   publicDir: 'public',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: './index.html'
-    }
-  },
-  server: {
-    port: 5173,
-    host: true
-  },
-  // Important for SPA routing
-  base: './',
+  }
 });
